14fd43f1131b7883145bea1f91baa7fe12c3713c
jorge luis de los santos reyes <jorge20080@hotmail.com>
2021-06-16 11:19:56 -0400

    z

---
+++ PalMercaditoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PalMercaditoApp.Startup))]
+namespace PalMercaditoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
